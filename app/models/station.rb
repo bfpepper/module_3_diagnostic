084fd44ce14822453a18127b4086d44d4e8d59e9
Ben Pepper <bfpepper87@gmail.com>
2016-12-14 10:12:26 -0700

    parse raw station data in model.

---
+++ app/models/station.rb
@@ -0,0 +1,12 @@
+class Station
+
+  def initialize(station)
+    binding.pry
+    @name = station[:station_name]
+    @address =
+    @fuel_type = 
+    @distance = station[:distance]
+    @access_times = station[:access_days_time]
+  end
+
+end
--- app/services/station_finder_services.rb
+++ app/services/station_finder_services.rb
@@ -8,12 +8,11 @@ class StationFinderServices
 
   def get_stations
     raw_stations = @connection.get("nearest.json?api_key=#{ENV['nrel_key']}&location=#{@zip_code}").body
-    parsed_stations = JSON.parse(raw_stations)
-    actual_stations = parsed_stations["fuel_stations"]
+    parsed_stations = JSON.parse(raw_stations, symbolize_names: true)
+    actual_stations = parsed_stations[:fuel_stations]
     actual_stations.map do |station|
       Station.new(station)
     end
   end
-
 
 end
