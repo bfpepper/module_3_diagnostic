be95701374d7b4359f371d9f76d88212e42161c2
Ben Pepper <bfpepper87@gmail.com>
2016-12-14 10:00:27 -0700

    add station service.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,8 @@
 class SearchController < ApplicationController
 
   def index
-    zip = params[:q]
-    raw_stations = Faraday.get("http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_key']}&location=zip").body
-    parsed_stations = JSON.parse(raw_stations)
     binding.pry
+    @stations = StationFinderServices.new(params[:q]).get_stations
   end
 
 end
---
+++ app/services/station_finder_services.rb
@@ -0,0 +1,17 @@
+class StationFinderServices
+  attr_reader :zip_code
+
+  def initialize(zip_code)
+    @zip_code = zip_code
+    @connection = Faraday.new("http://developer.nrel.gov/api/alt-fuel-stations/v1/")
+  end
+
+  def get_stations
+    raw_stations = Faraday.get(@connection.get("nearest.json?api_key=#{ENV['nrel_key']}&location=zip").body
+    parsed_stations = JSON.parse(raw_stations)
+    actual_stations = parsed_stations["fuel_stations"]
+binding.pry
+  end
+
+
+end
